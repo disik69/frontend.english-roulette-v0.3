2e66b057640028a64c0589ec74d09ac6006f327f
disik <disik69@rambler.ru>
2016-04-25 14:08:37 +0300

    backend config

--- src/app/index.constants.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    'use strict';
-
-    angular
-        .module('frontendEnglishRouletteV03')
-        .constant('backendUrl', 'http://localhost:8000');
-})();
---
+++ src/app/sample.index.constants.js
@@ -0,0 +1,11 @@
+/**
+ * Define 'backendUrl' and delete 'sample.' prefix.
+ */
+
+(function () {
+    'use strict';
+
+    angular
+        .module('frontendEnglishRouletteV03')
+        .constant('backendUrl', 'SOME_URL');
+})();
