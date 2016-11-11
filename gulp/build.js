4820d74f2e8510a70a286dfc10b9a3302590d626
disik <disik69@rambler.ru>
2016-11-11 10:04:50 +0200

    add less

--- gulp/build.js
+++ gulp/build.js
@@ -13,10 +13,11 @@ gulp.task('partials', function () {
             path.join(conf.paths.src, '/app/**/*.html'),
             path.join(conf.paths.tmp, '/serve/app/**/*.html')
         ])
-        .pipe($.minifyHtml({
-            empty: true,
-            spare: true,
-            quotes: true
+        .pipe($.htmlmin({
+            removeEmptyAttributes: true,
+            removeAttributeQuotes: true,
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true
         }))
         .pipe($.angularTemplatecache('templateCacheHtml.js', {
             module: 'frontendEnglishRouletteV03',
@@ -36,32 +37,31 @@ gulp.task('html', ['inject', 'partials'], function () {
     var htmlFilter = $.filter('*.html', {restore: true});
     var jsFilter = $.filter('**/*.js', {restore: true});
     var cssFilter = $.filter('**/*.css', {restore: true});
-    var assets;
 
     return gulp.src(path.join(conf.paths.tmp, '/serve/*.html'))
         .pipe($.inject(partialsInjectFile, partialsInjectOptions))
-        .pipe(assets = $.useref.assets())
-        .pipe($.rev())
+        .pipe($.useref())
         .pipe(jsFilter)
-        .pipe($.sourcemaps.init())
+        //.pipe($.sourcemaps.init())
         .pipe($.ngAnnotate())
         .pipe($.uglify({preserveComments: $.uglifySaveLicense})).on('error', conf.errorHandler('Uglify'))
-        .pipe($.sourcemaps.write('maps'))
+        .pipe($.rev())
+        //.pipe($.sourcemaps.write('maps'))
         .pipe(jsFilter.restore)
         .pipe(cssFilter)
-        .pipe($.sourcemaps.init())
-        .pipe($.minifyCss({processImport: false}))
-        .pipe($.sourcemaps.write('maps'))
+        //.pipe($.sourcemaps.init())
+        .pipe($.replace('../../bower_components/bootstrap/fonts/', '../fonts/'))
+        .pipe($.cssnano())
+        .pipe($.rev())
+        //.pipe($.sourcemaps.write('maps'))
         .pipe(cssFilter.restore)
-        .pipe(assets.restore())
-        .pipe($.useref())
         .pipe($.revReplace())
         .pipe(htmlFilter)
-        .pipe($.minifyHtml({
-            empty: true,
-            spare: true,
-            quotes: true,
-            conditionals: true
+        .pipe($.htmlmin({
+            removeEmptyAttributes: true,
+            removeAttributeQuotes: true,
+            collapseBooleanAttributes: true,
+            collapseWhitespace: true
         }))
         .pipe(htmlFilter.restore)
         .pipe(gulp.dest(path.join(conf.paths.dist, '/')))
@@ -84,7 +84,7 @@ gulp.task('other', function () {
 
     return gulp.src([
             path.join(conf.paths.src, '/**/*'),
-            path.join('!' + conf.paths.src, '/**/*.{html,css,js}')
+            path.join('!' + conf.paths.src, '/**/*.{html,css,js,less}')
         ])
         .pipe(fileFilter)
         .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
